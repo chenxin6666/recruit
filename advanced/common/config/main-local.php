246e36886fb27998b7b841a011a68496b72af1f0
weijie <576765439@qq.com>
2016-11-05 11:32:00 +0800

    //连接数据库

--- advanced/common/config/main-local.php
+++ advanced/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii',
+            'dsn' => 'mysql:host=localhost;dbname=recruit',
             'username' => 'root',
             'password' => 'root',
             'charset' => 'utf8',
